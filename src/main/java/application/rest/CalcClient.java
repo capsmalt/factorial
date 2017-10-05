6cd302a4e1599ac6b70df14614f95d2d07f3623e
capsmalt <capsmalt@gmail.com>
2017-10-05 15:22:11 +0900

    Modify indent using "format in Eclipse IDE"

--- src/main/java/application/rest/CalcClient.java
+++ src/main/java/application/rest/CalcClient.java
@@ -12,33 +12,31 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Dependent
 public class CalcClient {
-	
+
 	@Inject
-	@ConfigProperty(name="CALC_SERVICE_SERVICE_HOST", defaultValue="localhost")
-    private String calcHost;
+	@ConfigProperty(name = "CALC_SERVICE_SERVICE_HOST", defaultValue = "localhost")
+	private String calcHost;
 
-    @Inject
-    @ConfigProperty(name="CALC_SERVICE_SERVICE_PORT_HTTP", defaultValue="9080")
-    private String calcPort;
-    
-    private String calcServiceUrl;
+	@Inject
+	@ConfigProperty(name = "CALC_SERVICE_SERVICE_PORT_HTTP", defaultValue = "9080")
+	private String calcPort;
 
-    @PostConstruct
-    private void init() {
-        calcServiceUrl = "http://" + calcHost + ":" + calcPort + "/calc/rest/";
-        System.out.println( this.getClass().getSimpleName() + ": calcServiceUrl=" + calcServiceUrl ); 
-    }
+	private String calcServiceUrl;
 
-    public long multiply( long p1, long p2 ) {
-        System.out.println( this.getClass().getSimpleName() + ": p1=" + p1 + ", p2=" + p2 );
-        Client client = ClientBuilder.newClient();
-        WebTarget wt = client.target( calcServiceUrl );
-        JsonObject res  = wt.path( Long.toString( p1  ) )
-                            .path( Long.toString( p2 ) )
-                            .request( "application/json" )
-                            .get( JsonObject.class );
-        long product = res.getJsonNumber( "product" ).longValue();
-        System.out.println( this.getClass().getSimpleName() + ": p1=" + p1 + ", p2=" + p2 + ", product=" + product );
-        return product;
-    }
+	@PostConstruct
+	private void init() {
+		calcServiceUrl = "http://" + calcHost + ":" + calcPort + "/calc/rest/";
+		System.out.println(this.getClass().getSimpleName() + ": calcServiceUrl=" + calcServiceUrl);
+	}
+
+	public long multiply(long p1, long p2) {
+		System.out.println(this.getClass().getSimpleName() + ": p1=" + p1 + ", p2=" + p2);
+		Client client = ClientBuilder.newClient();
+		WebTarget wt = client.target(calcServiceUrl);
+		JsonObject res = wt.path(Long.toString(p1)).path(Long.toString(p2)).request("application/json")
+				.get(JsonObject.class);
+		long product = res.getJsonNumber("product").longValue();
+		System.out.println(this.getClass().getSimpleName() + ": p1=" + p1 + ", p2=" + p2 + ", product=" + product);
+		return product;
+	}
 }
